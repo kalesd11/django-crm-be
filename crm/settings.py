471d93cd5355774bc0e38170501ac07f4226f803
Suresh Kale <suresh.kale@datenwissen.com>
2026-02-21 11:28:01 +0530

    crm-01:updating req.txt with cors allowed headers

--- crm/settings.py
+++ crm/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+from corsheaders.defaults import default_headers
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -56,7 +56,7 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'crm.urls'
 CORS_ALLOW_ALL_ORIGINS = True
-
+CORS_ALLOW_HEADERS = list(default_headers)
 
 TEMPLATES = [
     {
